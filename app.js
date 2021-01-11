a2aa1e74c44850f1acc7d724ea9fe4797a99f671
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-11 14:52:11 -0800

    add initial alert for quiz and grab names

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-import { answerContainsY } from './utils.js';
+import { answerContainsY, hideElement, askQuestion } from './utils.js';
 
 const startQuizButton = document.getElementById('quiz-start-button');
 const resultsDisplayElement = document.getElementById('results-display');
-
+const quizContent = document.getElementById('hide-this');
 
 startQuizButton.addEventListener('click', () => {
+    //quiz introduction
+    alert('Thank you for participating! Hope you remembered all the juicy details');
+
+    //Get users first and last name
+    const fName = prompt('What is your first name?');
+    const lName = prompt('What is your Last name?');
+
     
 });
-// set event listeners to update state and DOM
+
--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
 export function answerContainsY(answerString) {
-    return answerString[0].toUppercase() === 'Y';
+    return answerString[0].toUpperCase() === 'Y';
+}
+export function hideElement(element){
+    element.classList.add('hidden');
+}
+export function askQuestion(questionString){
+    return prompt(questionString);
 }
