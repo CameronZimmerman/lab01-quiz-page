dc7404b4cabe30b4ccd24247348eb8988b9a00dc
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-11 17:00:07 -0800

    refactor tests and add test for percentage calculator function

--- test/example.test.js
+++ test/example.test.js
@@ -1,32 +1,47 @@
 // IMPORT MODULES under test here:
-import { answerContainsY } from '../utils.js';
+import { answerContainsY, calculatePercentage } from '../utils.js';
 
 const test = QUnit.test;
 
-test('function will return true when given string that starts with y', (expect) => {
+test('function will return true when given string that starts with correct char', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = answerContainsY('yabadabadoo');
+    const actual = answerContainsY('yabadabadoo', 'Y');
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
 
-test('function will return false when given string that starts with not y', (expect) => {
+test('function will return false when given string that starts with correct char', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = false;
+    const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = answerContainsY('nabadabadoo');
+    const actual = answerContainsY('nabadabadoo', 'N');
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('function will return a whole number percentage of the fraction from two numbers', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 67;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = calculatePercentage(2, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
